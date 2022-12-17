a0b49bab1ed9eab0c91150b363f075e39144fa02
FAN <ruby1103147@gmail.com>
2022-12-17 21:50:57 +0800

    signIn&signUp

--- js/function/customerFunction.js
+++ js/function/customerFunction.js
@@ -56,6 +56,37 @@ function logout() {
         }
 
     })
+}
 
+function signUp(strAccount, strPwd, strName, strPhone, intAge, strLineId, strEmail) {
+    let objPostData = { memberAccount: strAccount, memberPwd: strPwd, memberName: strName, memberPhone: strPhone, MemberAgeRange: intAge, memberLineId: strLineId, memberEmail: strEmail }
 
+    $.ajax({
+        url: 'http://localhost:8080/createMember',
+        method: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(objPostData),
+        success: function (res) {
+
+            let { message } = res
+
+            if (message === '所需資料缺失') {
+                alert('所需資料缺失')
+            } else if (message === '資料格式錯誤') {
+                alert('資料格式錯誤')
+            } else if (message === '帳號已存在') {
+                alert('帳號已存在')
+            } else if (message === 'Success') {
+                alert('註冊成功，請登入')
+                window.location.href = 'signIn.html'
+            }
+        }, xhrFields: {
+            withCredentials: true
+        },
+        error: function (e) {
+            console.log(e)
+            alert('Failed')
+        },
+    })
 }
---
+++ js/signUp.js
@@ -0,0 +1,18 @@
+$(document).on('click', '#signUpBtn', function (e) {
+    e.preventDefault()
+
+    let strAccount = $('#account').val()
+    let strPwd = $('#pwd').val()
+    let strName = $('#name').val()
+    let strPhone = $('#phone').val()
+    let intAge = $('#age').val()
+    let strLineId = $('#lineId').val()
+    let strEmail = $('#email').val()
+
+    if (strAccount == '' || strPwd == '' || strName == '' || strPhone == '') {
+        alert('請輸入必填項目')
+        return
+    }
+
+    signUp(strAccount, strPwd, strName, strPhone, intAge, strLineId, strEmail)
+})
