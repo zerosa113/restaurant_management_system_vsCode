dba1a6841855389444da1e92767fe95ae2cded58
yu <yu@example>
2023-01-03 11:36:36 +0800

    Update memberCheckout

--- js/checkout.js
+++ js/checkout.js
@@ -2,14 +2,28 @@ $(document).ready(function () {
 
     showShoppingCart()
     readPointsExchange()
+    memberPointInfo()
 })
 let intcostPoints = 0;
 
 $(document).on('click', '#pointsExchangeBtn', function (e) {
     e.preventDefault()
     // shoppingCart()
+    let checkCostPoints = parseInt($('#costPoints').val())
+    let memberPoints = parseInt($('#memberInfoPoints').html())
 
-    intcostPoints = $('#costPoints').val()
+    // console.log(checkCostPoints)
+    // console.log(memberPoints)
+
+    if (checkCostPoints <= 0) {
+        alert('輸入點數小於等於零')
+    } else if (checkCostPoints > memberPoints) {
+        alert('輸入點數大於會員點數')
+    } else if (checkCostPoints > 0) {
+        intcostPoints = $('#costPoints').val()
+        alert('輸入點數：' + intcostPoints)
+    }
+
     console.log(intcostPoints)
     // let jsonCostPoints = {}
     // jsonCostPoints["costPoints"] = inputCostPoints
